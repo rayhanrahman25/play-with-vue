45b55d5bee54e3fabd224408c6b7222b509434d9
rayhanrahman25 <rayhanrahman880@gmail.com>
2023-08-08 20:29:52 +0600

    learned about components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
 
  const app = createApp(App)
+ app.component('food-item', FoodItem)
  app.mount('#app')
